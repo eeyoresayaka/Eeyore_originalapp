b61bfe5929f272b117df3abfc2e1cf2e6d80dd0b
sayaka <sayaka.rinrin0723@gmail.com>
2023-05-28 23:30:41 +0900

    カレンダー画面の修正

--- Eeyore_originalapp/CalendarViewController.swift
+++ Eeyore_originalapp/CalendarViewController.swift
@@ -17,18 +17,26 @@ class CalendarViewController: UIViewController, FSCalendarDelegate, FSCalendarDa
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let calendar = FSCalendar(frame: CGRect(x: 25, y: 120, width: 350, height: 300))
+        let calendar = FSCalendar(frame: CGRect(x: 25, y: 110, width: 350, height: 300))
         calendar.dataSource = self
         calendar.delegate = self
         view.addSubview(calendar)
-        calendar.appearance.weekdayTextColor = UIColor.systemPink
+//        calendar.appearance.weekdayTextColor = UIColor.systemPink
+        calendar.appearance.weekdayTextColor = .darkGray //曜日表示のテキストカラー
+        calendar.appearance.titleWeekendColor = .systemPink
         calendar.appearance.headerTitleColor = UIColor.systemPink
-        label = UILabel(frame: CGRect(x: 20, y: 300, width: 320, height: 300))
+        calendar.appearance.headerTitleFont      = UIFont.boldSystemFont(ofSize: 22)
+        calendar.appearance.headerDateFormat = "yyyy年MM月"
+        calendar.appearance.headerMinimumDissolvedAlpha = 0
+        calendar.appearance.weekdayFont          = UIFont.systemFont(ofSize: 17)
+        calendar.appearance.titleFont            = UIFont.systemFont(ofSize: 17)
+        label = UILabel(frame: CGRect(x: 25, y: 285, width: 320, height: 300))
         view.addSubview(label)
     }
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         df.dateFormat = "yyyy年MM月dd日"
         label.text = df.string(from: date)
+        label.font = UIFont.systemFont(ofSize: 23.0)
     }
 }
